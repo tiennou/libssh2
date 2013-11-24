f24ddc4553f67a9ce786d5b5a20bb31c15b538bd
Keith Duncan <keith.duncan@github.com>
2013-11-24 17:01:52 +0000

    Implement the hash functions

--- src/securetransport.h
+++ src/securetransport.h
@@ -1,27 +1,28 @@
 /* Copyright (C) 2013 Keith Duncan */
 
 #import <Security/Security.h>
+#import <CommonCrypto/CommonCrypto.h>
 
 #define _libssh2_random(buf, len) SecRandomCopyBytes(kSecRandomDefault, len, buf)
 
-#define libssh2_sha1_ctx
-#define libssh2_sha1_init(ctx)
-#define libssh2_sha1_update(ctx, data, len)
-#define libssh2_sha1_final(ctx, out)
-void libssh2_sha1(const unsigned char *message, unsigned long len, unsigned char *out);
+#define libssh2_sha1_ctx CC_SHA1_CTX
+#define libssh2_sha1_init(ctx) CC_SHA1_Init(ctx)
+#define libssh2_sha1_update(ctx, data, len) CC_SHA1_Update(ctx, data, len)
+#define libssh2_sha1_final(ctx, out) CC_SHA1_Final(out, ctx)
+#define libssh2_sha1(data, datalen, out) CC_SHA1(data, datalen, out)
 
-#define libssh2_md5_ctx
-#define libssh2_md5_init(ctx)
-#define libssh2_md5_update(ctx, data, len)
-#define libssh2_md5_final(ctx, out)
-void libssh2_md5(const unsigned char *message, unsigned long len, unsigned char *out);
+#define libssh2_md5_ctx CC_MD5_CTX
+#define libssh2_md5_init(ctx) CC_MD5_Init(ctx)
+#define libssh2_md5_update(ctx, data, len) CC_MD5_Update(ctx, data, len)
+#define libssh2_md5_final(ctx, out) CC_MD5_Final(out, ctx)
+#define libssh2_md5(data, datalen, out) CC_MD5(data, datalen, out)
 
-#define libssh2_hmac_ctx
-#define libssh2_hmac_sha1_init(ctx, key, keylen)
-#define libssh2_hmac_md5_init(ctx, key, keylen)
-#define libssh2_hmac_ripemd160_init(ctx, key, keylen)
-#define libssh2_hmac_update(ctx, data, datalen)
-#define libssh2_hmac_final(ctx, data)
+#define libssh2_hmac_ctx CCHmacContext
+#define libssh2_hmac_sha1_init(ctx, key, keylen) CCHmacInit(ctx, kCCHmacAlgSHA1, key, keylen)
+#define libssh2_hmac_md5_init(ctx, key, keylen) CCHmacInit(ctx, kCCHmacAlgMD5, key, keylen)
+//#define libssh2_hmac_ripemd160_init(ctx, key, keylen)
+#define libssh2_hmac_update(ctx, data, datalen) CCHmacUpdate(ctx, data, datalen)
+#define libssh2_hmac_final(ctx, data) CCHmacFinal(ctx, data)
 #define libssh2_hmac_cleanup(ctx)
 
 #define libssh2_crypto_init()
@@ -40,15 +41,9 @@ void libssh2_md5(const unsigned char *message, unsigned long len, unsigned char 
 #define _libssh2_cipher_aes192
 #define _libssh2_cipher_aes128
 
-#ifdef HAVE_EVP_AES_128_CTR
 #define _libssh2_cipher_aes128ctr
 #define _libssh2_cipher_aes192ctr
 #define _libssh2_cipher_aes256ctr
-#else
-#define _libssh2_cipher_aes128ctr
-#define _libssh2_cipher_aes192ctr
-#define _libssh2_cipher_aes256ctr
-#endif
 
 #define _libssh2_cipher_blowfish
 #define _libssh2_cipher_arcfour
