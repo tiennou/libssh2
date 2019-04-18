97f55daa2e20a6a1689adb740ca07dd12b781f08
Etienne Samson <samson.etienne@gmail.com>
2019-04-18 19:08:32 +0200

    Switch Travis to 16.04

--- ci/setup.sh
+++ ci/setup.sh
@@ -4,21 +4,21 @@ set -e
 
 # Fix issue with chrome and 32-bit multilib
 # See http://www.omgubuntu.co.uk/2016/03/fix-failed-to-fetch-google-chrome-apt-error-ubuntu
-sudo sed -i -e 's/deb http/deb [arch=amd64] http/' "/etc/apt/sources.list.d/google-chrome.list"
-sudo sed -i -e 's/deb http/deb [arch=amd64] http/' "/opt/google/chrome/cron/google-chrome"
+# sudo sed -i -e 's/deb http/deb [arch=amd64] http/' "/etc/apt/sources.list.d/google-chrome.list"
+# sudo sed -i -e 's/deb http/deb [arch=amd64] http/' "/opt/google/chrome/cron/google-chrome"
 
 if [ $ADDRESS_SIZE = '32' ]; then
     sudo dpkg --add-architecture i386
     sudo apt-get update -qq
     sudo apt-get install -y gcc-multilib
-    sudo apt-get install -y libssl-dev:i386 libgcrypt11-dev:i386 build-essential gcc-multilib
+    sudo apt-get install -y libssl-dev:i386 libgcrypt-dev:i386 build-essential gcc-multilib
     sudo dpkg --purge --force-depends gcc-multilib && sudo dpkg --purge --force-depends libssl-dev
 
     export CMAKE_FLAGS="-DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchain-Linux-32.cmake"
 elif [ $ADDRESS_SIZE = '64' ]; then
     sudo apt-get update -qq
     sudo apt-get install -y libssl-dev
-    sudo apt-get install -y libgcrypt11-dev
+    sudo apt-get install -y libgcrypt-dev
 fi
 
 if [ $LEAK_CHECK = 'valgrind' ]; then
